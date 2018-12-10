05c07ff88045729a6a835c4e2650a8efbce61519
Sriram Murali <Sriram.Murali@hsrw.org>
2018-12-10 16:57:05 +0100

    Commit for Rohr Changes
    Commit for Rohr Changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,29 +3,28 @@ import './Header.css';
 
 const Header = () =>{
         return(
-
             <div className="headerContainer">
-              <div className="col"></div>
-              <nav className="navbar navbar-expand-lg navbar-light col-6">
-              <div className="collapse navbar-collapse">
-              <ul className="nav container">
-                <li className="nav-item">
-                    <a className="nav-link textSpan" href="#"><b>ENI App</b></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link textSpan" href="#">Rohrleitungen</a>
-                </li>
-                <li className="nav-item ">
-                    <a className="nav-link textSpan" href="#">Inspektron</a>
-                </li>
-                <li className="nav-item form-inline mx-auto">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Suche" aria-label="Search"/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Suchen</button>
-                </li>
-                </ul>
+            <div className="container">
+              <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="collapse navbar-collapse">
+                    <ul className="nav row">
+                        <li className="nav-item">
+                            <a className="nav-link textSpan" href="#"><b>ENI App</b></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link textSpan" href="#">Rohrleitungen</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link textSpan" href="#">Inspektron</a>
+                        </li>
+                        <li className="nav-item form-inline ml-auto">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Suche" aria-label="Search"/>
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Suchen</button>
+                        </li>
+                    </ul>
                 </div>
               </nav>
-              <div className="col"></div>
+            </div>
             </div>
         );
 };
--- src/Components/Rohrleitungen/Rohrleitungen.js
+++ src/Components/Rohrleitungen/Rohrleitungen.js
@@ -1,44 +1,96 @@
 import React from 'react';
 import './Rohrleitungen.css';
+import details from './details.png';
 
 
 const container = () =>{
     return(
         <div className="container gridContainer">
-        <div>
-            <h1>Rohrleitungen</h1>
-            <button type="submit" className="btn btn-primary float-right">Upload</button>
-        </div>
-        <table className="table">
-            <thead className="thead-dark">
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                 <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                 <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                 <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
+            <div>
+                <h1 className="d-inline-block">Rohrleitungen</h1>
+                <button type="submit" className="btn btn-primary float-right">Upload von Rohrleitungen</button>
+            </div><br/><br/>
+            <div className="tableContainer">
+            <table className="table">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Rohrleitungen ID</th>
+                        <th scope="col">Messpunkte</th>
+                        <th scope="col">Prüfaufsicht</th>
+                        <th scope="col">Letzte Inspektion</th>
+                        <th scope="col">Inspektionen</th>
+                        <th scope="col">Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                        <td>D015-390A-O-022108</td>
+                        <td>6</td>
+                        <td>ZUS</td>
+                        <td>Keine</td>
+                        <td>0</td>
+                        <td><img src={process.env.PUBLIC_URL + '/details.png'} className="detailsImage" alt="..." /></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                        <td>D015-390A-O-022108</td>
+                        <td>6</td>
+                        <td>ZUS</td>
+                        <td>Keine</td>
+                        <td>0</td>
+                        <td><img src={process.env.PUBLIC_URL + '/details.png'} className="detailsImage" alt="..." /></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                        <td>D015-390A-O-022108</td>
+                        <td>6</td>
+                        <td>ZUS</td>
+                        <td>Keine</td>
+                        <td>0</td>
+                        <td><img src={process.env.PUBLIC_URL + '/details.png'} className="detailsImage" alt="..." /></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">4</th>
+                        <td>D015-390A-O-022108</td>
+                        <td>6</td>
+                        <td>ZUS</td>
+                        <td>Keine</td>
+                        <td>0</td>
+                        <td><img src={process.env.PUBLIC_URL + '/details.png'} className="detailsImage" alt="..." /></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">5</th>
+                        <td>D015-390A-O-022108</td>
+                        <td>6</td>
+                        <td>ZUS</td>
+                        <td>Keine</td>
+                        <td>0</td>
+                        <td><img src={process.env.PUBLIC_URL + '/details.png'} className="detailsImage" alt="..." /></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">6</th>
+                        <td>D015-390A-O-022108</td>
+                        <td>6</td>
+                        <td>ZUS</td>
+                        <td>Keine</td>
+                        <td>0</td>
+                        <td><img src={process.env.PUBLIC_URL + '/details.png'} className="detailsImage" alt="..." /></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">7</th>
+                        <td>D015-390A-O-022108</td>
+                        <td>6</td>
+                        <td>ZUS</td>
+                        <td>Keine</td>
+                        <td>0</td>
+                        <td><img src={process.env.PUBLIC_URL + '/details.png'} className="detailsImage" alt="..." /></td>
+                    </tr>              
+                </tbody>
+            </table>
+            </div><br/><br/>
+        <button type="button" className="btn btn-success btnDownloadAllInspection float-right">Alle Inspektionen downloaden</button>
         </div>
     )
 };
